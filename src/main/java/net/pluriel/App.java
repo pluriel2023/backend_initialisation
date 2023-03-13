6859e0c670c69dc48a1ccbcac8b7d513afef79fc
LAGRIDA <lagyassine@gmail.com>
2023-03-13 12:16:59 +0100

    initialisation du cours Java

---
+++ src/main/java/net/pluriel/App.java
@@ -0,0 +1,61 @@
+package net.pluriel;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import net.pluriel.classes.User;
+
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        
+//        User user1 = User.builder().firstId(1).secondeId(2).name("Ahmed").build();
+//        User user2 = User.builder().firstId(3).secondeId(4).name("Karim").build();
+//        User user3 = User.builder().firstId(5).secondeId(6).name("Ahmed").build();
+//        
+//        List<User> users = Arrays.asList(user1, user2, user3);
+//        
+//        //Set<Integer> ids = users.stream().map(el -> { return el.getFirstId(); }).collect(Collectors.toSet());
+//        
+//        List<User> filterdUsers = users.stream().filter(el -> el.getName().equals("Ahmed")).collect(Collectors.toList());
+//        
+//        Set<Integer> ids = filterdUsers.stream().map(el -> el.getFirstId()).collect(Collectors.toSet());
+//        
+//        Optional<User> findUserOpt = users.stream().filter(el -> el.getName().equals("Ahmed")).findFirst();
+//        
+//        if(findUserOpt.isPresent()) {
+//        	User findUser = findUserOpt.get();
+//        	System.out.println(findUser);
+//        }
+//        if(findUserOpt.isEmpty()) {
+//        	System.out.println("------> paas de resultat");
+//        }
+        
+//        float a = 1.7f;
+//        
+//        float b = 1.5f;
+//        
+//        float c = a - b;
+//        
+//        System.out.println(c);
+//        System.out.println(c == 0.2f);
+//        
+//        BigDecimal x = new BigDecimal("1.7");
+//        
+//        BigDecimal y = new BigDecimal("1.5");
+//        
+//        BigDecimal diffrence = x.subtract(y);
+//        
+//        System.out.println(diffrence.compareTo(new BigDecimal("0.2")) == 0);
+        
+        
+    }
+}
